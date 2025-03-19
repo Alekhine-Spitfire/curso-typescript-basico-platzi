830d77efb99dd906740937751fec9ae803c4354f
Alekhine Terrazas <enrique01346@gmail.com>
2025-03-19 12:06:53 -0500

    Clase 4 completadaa y he añadido un .gitignore

---
+++ Variables_diferentes/main.ts
@@ -0,0 +1,44 @@
+// ####################### any #######################
+
+
+let variableAny: any;
+
+// variableAny = "hello";     // Cadena de texto
+variableAny = 42;          // Número
+//variableAny = true;        // Booleano
+console.log("Valor de variable any " + variableAny)
+
+
+// ####################### unknown #######################
+
+
+let variableDesconocida: unknown;
+
+// variableDesconocida = "hello";
+variableDesconocida = 42;
+// variableDesconocida = true;
+
+if (typeof variableDesconocida === "string") {
+    console.log("Variable unknown es una cadena:", variableDesconocida);
+}
+
+
+// ####################### never #######################
+
+
+/* function arrojarError(mensaje: string): never {
+    throw new Error(mensaje);
+}
+
+arrojarError("Esto es un error"); */
+
+
+// ####################### void #######################
+
+function logMessage(message: string): void {
+    console.log(message);
+    // No hay return
+}
+
+logMessage("Este es un mensaje de log");
+
