df4cc9ea6466e48d2ca06490d1752e4aa35e9bf0
Alekhine Terrazas <enrique01346@gmail.com>
2025-03-19 11:27:02 -0500

    Clase 3 del curso de TypeScript de Platzi completadaa

---
+++ Variable_base/main.js
@@ -0,0 +1,10 @@
+var nombre = "Alekhine";
+var edad = 29;
+var esDesarrollador = true;
+var mes;
+var variableNull = null;
+console.log("Mes inicial:", mes);
+mes = "Marzo";
+console.log("Mes actualizado:", mes);
+console.log("Hola mundooou! " + nombre);
+console.log("Hola mundooou! ".concat(nombre));
---
+++ Variable_base/main.ts
@@ -0,0 +1,14 @@
+let nombre: string = "Alekhine";
+let edad: number = 29;
+let esDesarrollador: boolean = true;
+let mes: string | undefined;
+let variableNull: null = null;
+
+console.log("Mes inicial:", mes);
+
+mes = "Marzo";
+console.log("Mes actualizado:", mes)
+
+console.log("Hola mundooou! " + nombre);
+
+console.log(`Hola mundooou! ${nombre}`)
